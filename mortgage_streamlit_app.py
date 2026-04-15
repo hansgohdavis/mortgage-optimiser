8ffdd2ee135bdb173a100a6e730376ce5f6e427e
hansgohdavis <hans.gohdavis@gmail.com>
2026-04-15 21:21:01 +1000

    mortgage_streamlit_app.py

--- mortgage_streamlit_app.py
+++ mortgage_streamlit_app.py
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Loan Refinance Optimiser • Production-Grade Streamlit</title>
+    <title>Loan Refinance Optimiser Production-Grade Streamlit</title>
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&amp;family=Space+Grotesk:wght@500;600&amp;display=swap');
         
